983f62bad349822e957d985510d68711e2dce0ee
xiaoming <156246158@qq.com>
2020-03-06 23:05:21 +0800

    feat: 添加辅助方法-remove-删除数组元素

--- src/lib/util/_.ts
+++ src/lib/util/_.ts
@@ -1,3 +1,21 @@
+/**
+ * 判断参数值是否为对象
+ * @param param 待判断参数
+ */
 export const isObject = (param: any): boolean => {
   return param !== null && typeof param === 'object'
+}
+
+/**
+ * 从数组中删除指定项
+ * @param arr 数组
+ * @param item 待删除项
+ */
+export const remove = (arr: Array<any>, item: any): Array<any> | void => {
+  if (arr.length) {
+    const index = arr.indexOf(item)
+    if (index !== -1) {
+      return arr.splice(index, 1)
+    }
+  }
 }
