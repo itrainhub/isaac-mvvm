ace789f8d51949f2e9f921ee8efe5614b4bf977b
xiaoming <156246158@qq.com>
2020-03-06 23:12:56 +0800

    feat: 添加Dep-数据更新的发布者

---
+++ src/lib/dep.ts
@@ -0,0 +1,50 @@
+import { remove } from './util'
+
+// 全局编号
+let uid: number = 0
+
+/**
+ * 数据更新的发布者
+ * get 数据的时候，收集订阅者（订阅该数据的 Watcher）
+ * set 数据的时候，通知订阅者
+ */
+export default class Dep {
+  static target: Watcher
+  id: number
+  subs: Array<Watcher>
+
+  constructor() {
+    this.id = uid++
+    this.subs = []
+  }
+
+  /**
+   * 添加订阅者
+   * @param sub 订阅者对象
+   */
+  addSub = (sub: Watcher): void => {
+    this.subs.push(sub)
+  }
+
+  /**
+   * 删除订阅者
+   * @param sub 订阅者对象
+   */
+  removeSub = (sub: Watcher): void => {
+    remove(this.subs, sub)
+  }
+
+  /**
+   * 收集订阅者
+   */
+  depend = (): void => {
+    Dep.target && Dep.target.addDep(this)
+  }
+
+  /**
+   * 通知订阅者
+   */
+  notify = (): void => {
+    this.subs.forEach(sub => sub.update())
+  }
+}
