c0adad67e8cf9eaf0001945cb834f54f68506d9e
xiaoming <156246158@qq.com>
2020-03-07 15:46:09 +0800

    feat: 添加 x-model 指令解析功能

--- src/lib/compiler/directive-handler.ts
+++ src/lib/compiler/directive-handler.ts
@@ -17,6 +17,10 @@ export default {
     new Watcher(vm, expression, (value, oldValue) => {
       fn && fn(node, value, oldValue)
     })
+
+    if (directive === 'model') {
+      this.handleInput(node, vm, expression)
+    }
   },
 
   /**
@@ -32,10 +36,53 @@ export default {
   },
 
   /**
+   * 设置挂载在 vm 上的属性值
+   * @param vm vm
+   * @param props 属性名
+   * @param value 属性值
+   */
+  setVmValue(vm: ViewModel, props: string, value: any): void {
+    const exps = props.split('.')
+    const maxIndex = exps.length - 1
+    exps.forEach((key, index) => {
+      if (index < maxIndex) {
+        vm = vm[key]
+        return
+      }
+      vm[key] = value
+    })
+  },
+
+  /**
    * 处理文本
    */
   processText(node: Node, value: string | undefined): void {
     node.textContent = typeof value === 'undefined' ? '' : value
+  },
+
+  /**
+   * 处理 model 绑定
+   * @param node 节点
+   * @param value 值
+   */
+  processModel(node: Node, value: string | undefined): void {
+    (<HTMLInputElement>node).value = typeof value === 'undefined' ? '' : value
+  },
+
+  /**
+   * 文本框 x-model 处理 input 事件
+   * @param node 节点
+   * @param vm ViewModel
+   * @param expression 表达式
+   */
+  handleInput(node: Node, vm: ViewModel, expression: string) {
+    let val = this.getVmValue(vm, expression)
+    node.addEventListener('input', () => {
+      const value = (<HTMLInputElement>node).value
+      if (val === value) return
+      this.setVmValue(vm, expression, value)
+      val = value
+    }, false)
   },
 
   /**
