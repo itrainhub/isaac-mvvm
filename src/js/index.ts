6cc8463605bcd69878a3fe9f2a7a1de13c7cb16d
xiaoming <156246158@qq.com>
2020-03-14 00:57:52 +0800

    test: 测试demo

--- src/js/index.ts
+++ src/js/index.ts
@@ -11,16 +11,44 @@ const vm = new ViewModel({
         middle: '小',
         last: '王'
       },
-      age: 18
+      age: 18,
+      hobbies: ['吃饭', '睡觉', '打豆豆']
     },
-    hobbies: ['吃饭', '睡觉', '打豆豆']
+    cart: [
+      {
+        id: 1,
+        title: 'prod-1',
+        price: {
+          original: 99,
+          cost: 9.9
+        },
+        discount: 0.8,
+        amount: 1
+      },
+      {
+        id: 2,
+        title: 'prod-2',
+        price: {
+          original: 9.9,
+          cost: 6.5
+        },
+        discount: 1,
+        amount: 10
+      }
+    ]
   },
   methods: {
-    handle() {
+    handleChangeMsg() {
       this['msg'] = 'changed'
     },
     handleAddData() {
-      this['hobbies'].unshift(Math.random())
+      this['stu']['hobbies'].unshift(Math.random())
+    },
+    calcPayment() {
+      return this['cart'].reduce((sum: number, prod: any) => (sum += prod.price.original * prod.discount * prod.amount), 0)
+    },
+    handleModify(index: number) {
+      this['cart'][index].price.original = 9.9
     }
   }
 })
